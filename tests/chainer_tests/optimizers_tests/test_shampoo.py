f6998ede0371ec3dd2c3dc2e2c858e84bc364ac6
Toshiki Kataoka <kataoka@preferred.jp>
2018-03-06 18:30:35 +0900

    test matrix power

---
+++ tests/chainer_tests/optimizers_tests/test_shampoo.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy
+import scipy
+
+from chainer.optimizers import shampoo
+from chainer import testing
+
+
+class TestFractionalMatrixPower(unittest.TestCase):
+
+    def test1(self):
+        x = numpy.random.randn(3, 3)
+        x += x.T  # test symmetric matrix
+        a = x
+        a = a.dot(a)
+        a = a.dot(a)
+        t = 0.25
+        numpy.testing.assert_allclose(
+            shampoo._fractional_matrix_power(a, t),
+            scipy.linalg.fractional_matrix_power(a, t))
+
+
+testing.run_module(__name__, __file__)
