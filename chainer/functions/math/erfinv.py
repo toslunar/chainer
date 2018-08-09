8c6e31e04cb6865176047146fdef21de64e57c71
Toshiki Kataoka <kataoka@preferred.jp>
2018-08-09 18:18:50 +0900

    Move erfcinv

--- chainer/functions/math/erfinv.py
+++ chainer/functions/math/erfinv.py
@@ -62,3 +62,51 @@ def erfinv(x):
         ~chainer.Variable: Output variable.
     """
     return ErfInv().apply((x,))[0]
+
+
+class ErfcInv(function_node.FunctionNode):
+
+    @property
+    def label(self):
+        return 'erfcinv'
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        type_check.expect(in_types[0].dtype.kind == 'f')
+
+    def forward_cpu(self, x):
+        if not available_cpu:
+            raise ImportError("SciPy is not available. Forward computation"
+                              " of erfcinv in CPU can not be done." +
+                              str(_import_error))
+        self.retain_outputs((0,))
+        return utils.force_array(special.erfcinv(x[0]), dtype=x[0].dtype),
+
+    def forward_gpu(self, x):
+        self.retain_outputs((0,))
+        return cuda.elementwise(
+            'T x', 'T y',
+            'y = erfcinv(x)',
+            'elementwise_erfcinv',
+        )(x[0]),
+
+    def backward(self, indexes, gy):
+        y, = self.get_retained_outputs()
+        return -BACKWORDC * chainer.functions.exp(y ** 2) * gy[0],
+
+
+def erfcinv(x):
+    """Elementwise inverse function of complementary error function.
+
+    .. note::
+       Forward computation in CPU can not be done if
+       `SciPy <https://www.scipy.org/>`_ is not available.
+
+    Args:
+        x (:class:`~chainer.Variable` or :class:`numpy.ndarray` or \
+        :class:`cupy.ndarray`): Input variable.
+
+    Returns:
+        ~chainer.Variable: Output variable.
+    """
+    return ErfcInv().apply((x,))[0]
